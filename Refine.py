c51a997d3fe4c440f54d11c297500820a5aa181e
Valerian Coelho <valerian27012004@gmail.com>
2024-01-22 22:16:25 +0530

    File Names Refined

--- Refine.py
+++ Refine.py
@@ -2,5 +2,7 @@ from Windows import get_file_names
 
 directory_path = 'D:\\Valerian\\Coding\\Python\\Personal Projects\\Index Shifter\\SongTest'
 file_names = get_file_names(directory_path)
-for name in file_names:
-    print(name)
+for i in range(len(file_names)):
+    new_file_name = str(i+1).zfill(3) + ' ' + file_names[i].split(maxsplit=1)[1]
+    print(new_file_name)
+    
